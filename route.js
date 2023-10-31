0c1677d586973d53e1618dbc31539ea1de47630d
JimChen <tc21122402@tzuchi.com.tw>
2023-10-31 14:09:52 +0800

    (柏鈞) add 404 not found image

--- route.js
+++ route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require('fs');
 
 app.use(express.static("public"));
 
@@ -9,7 +10,16 @@ app.get("/", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-	res.send("404 Not Found.");
+  fs.readFile('public/images/404notfound.jpg', (err, data) => {
+    if (err) {
+      // 如果出現錯誤，發送錯誤訊息
+      res.send("404 Not Found.");
+    } else {
+      // 發送圖像作為響應
+      res.contentType('image/jpeg'); // 設置響應類型為圖像/jpeg
+      res.send(data); // 發送圖像數據
+    }
+  });
 }); 
 
 app.listen(3000, () => {
