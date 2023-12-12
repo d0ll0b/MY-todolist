b742ee93c58d63702c4e5bb12c9a9f73eed7a63d
JimChen <tc21122402@tzuchi.com.tw>
2023-12-12 12:03:01 +0800

    (dOllOb) 修改todo樣式為正方形

--- public/app.js
+++ public/app.js
@@ -2,38 +2,6 @@ let section = document.querySelector("section");
 let add = document.querySelector("form button");
 add.addEventListener("click", handleClick);
 
-// add.addEventListener("click", e =>{
-//   e.preventDefault(); 
-//   //prevent form submitted 避免form送出
-  
-//   let form = e.target.parentElement;
-//   let todoText = form.children[0].value;
-//   if(todoText!='' && typeof todoText === 'string'){
-//     //create a todo 
-//     let todo = document.createElement("div");
-//     todo.classList.add("todo");
-//     let text = document.createElement("p");
-//     todo.classList.add("todo-text");
-//     text.innerText = todoText
-//     todo.appendChild(text);
-//     document.querySelector(".input_text").value = '';
-    
-//     // check
-//     let completeButton = document.createElement("button");
-//     completeButton.classList.add("complete");
-//     completeButton.innerHTML = '<i class="fas fa-check"></i>';
-//     todo.appendChild(completeButton);
-    
-//     // remove
-//     let trashButton = document.createElement("button");
-//     trashButton.classList.add("trash");
-//     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-//     todo.appendChild(trashButton);
-
-//     section.appendChild(todo);
-//   }
-// })
-
 function handleClick(e) {
   e.preventDefault(); 
   //prevent form submitted 避免form送出
@@ -48,20 +16,20 @@ function handleClick(e) {
     text.classList.add("todo-text");
     text.innerText = todoText
     document.querySelector(".input_text").value = '';
+    todo.appendChild(text);
     
     // check
     let completeButton = document.createElement("button");
     completeButton.classList.add("complete");
     completeButton.innerHTML = '<i class="fas fa-check"></i>';
-    text.appendChild(completeButton);
+    todo.appendChild(completeButton);
     
     // remove
     let trashButton = document.createElement("button");
     trashButton.classList.add("trash");
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    text.appendChild(trashButton);
+    todo.appendChild(trashButton);
 
-    todo.appendChild(text);
 
     section.appendChild(todo);
   }
